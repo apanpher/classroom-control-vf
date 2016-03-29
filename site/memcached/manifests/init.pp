87d36e7da04a360386465d18b015eb84029fc2c6
apanpher <anu@panpher.com>
2016-03-29 13:44:30 -0600

    Update init.pp

--- site/memcached/manifests/init.pp
+++ site/memcached/manifests/init.pp
@@ -1,18 +1,22 @@
 
 class memcached {
-
-## Manage the package
-package { memcached :
-    ensure =>present,
+  ## Manage the package
+  package { 'memcached':
+    ensure => present,
   }
-
-## Manage the file
-file { '/etc/sysconfig/memcached':
-    ensure =>file,
+  
+  ## Manage the file
+  file { '/etc/sysconfig/memcached':
+    ensure  => file,
+    source  => 'puppet:///modules/memcached/memcached',
+    require => Package['memcached'],
+    notify  => Service['memcached'],
   }
-
-## Manage the service
-
+  
+  ## Manage the service
+  service { 'memcached':
+    ensure => running,
+    enable => true,
+  }
 }
-
 
