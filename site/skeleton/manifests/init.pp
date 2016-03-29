2b720e8b6798381477281b62fbab4bf6a112af84
apanpher <anu@panpher.com>
2016-03-29 11:15:24 -0600

    Update init.pp

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -1,5 +1,8 @@
 class skeleton {
 
-## Manage /etc/skeleton/manifest
+## Manage /etc/skel directory
+## Manage /etc/skel/.bashrc file
+## NOTE:Source the file from the module
+## NOTE: Copy from /etc/skel/.bashrc in your container and modify it a bit
 
 }
