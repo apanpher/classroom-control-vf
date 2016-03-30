e483041b3eece49360a654f2b537eed47454e2af
apanpher <anu@panpher.com>
2016-03-30 10:20:40 -0600

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -7,14 +7,14 @@ class nginx {
     ensure => installed,
   }
   
-  file { "{$loc}nginx.conf":
+  file { "${loc}nginx.conf":
     ensure => file,
     source => 'puppet:///modules/nginx/nginx.conf',
     require => Package['nginx'],
     notify  => Service['nginx'],
   }
   
-  file { '{$loc}/conf.d/default.conf':
+  file { '${loc}/conf.d/default.conf':
     ensure => file,
     source => 'puppet:///modules/nginx/default.conf',
     require => Package['nginx'],
